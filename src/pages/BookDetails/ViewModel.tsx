b40295b18d2d27a3833c182bda4ec0e54c511f69
Lil-Tanatat <tas.tanatat@gmail.com>
2025-02-14 14:51:00 +0700

    chore: update API endpoints in .env and modify fallback URL in ViewModel

--- src/pages/BookDetails/ViewModel.tsx
+++ src/pages/BookDetails/ViewModel.tsx
@@ -18,7 +18,7 @@ const ViewModel = () => {
     }, [id, getBookById]);
 
     const AppDeepLink = 'exp+deer-and-book-pdf-reader://expo-development-client/?url=http%3A%2F%2F192.168.1.108%3A8081';
-    const fallbackUrl = 'https://play.google.com/store/apps/details?id=com.Slack';
+    const fallbackUrl = 'https://expo.dev/accounts/liltanatat/projects/deer-and-book-pdf-reader/builds/2f8ab7f4-5a09-46c4-8896-b2a231704171';
 
     const handleOpenApp = () => {
         const link = document.createElement('a');
